7774dd08aa36e75f6fb7fc3df42e1670da82f99c
RafaBrito008 <rbrito9162@uta.edu.ec>
2023-09-26 18:33:38 -0500

    Navbar y footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Logo from "../img/logo_fisei.png";
 
 const Footer = () => {
     return (
-        <div>Footer</div>
-    )
-}
+        <footer>
+            <img src={Logo} alt="" />
+            <span>
+                <b>React.js</b>.
+            </span>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from "../img/logo_uta.png"
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='navbar'>
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">
+                        <img src={Logo} alt="" />
+                    </Link>
+                </div>
+                <div className="links">
+                    <Link className="link" to="/?cat=categoria1">
+                        <h6>CATEGORIA1</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=categoria2">
+                        <h6>CATEGORIA2</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=categoria3">
+                        <h6>CATEGORIA3</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=categoria4">
+                        <h6>CATEGORIA4</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=categoria5">
+                        <h6>CATEGORIA5</h6>
+                    </Link>
+                    <span>Rafael</span>
+                    <span>Salir</span>
+                    <span className='write'><Link to="/write">Postear</Link></span>
+                </div>
+            </div>
+        </div>
     )
 }
 
