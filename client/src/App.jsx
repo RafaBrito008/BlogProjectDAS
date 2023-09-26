0551fa6a7585bd103c0ed5074cd1da4451f81d53
RafaBrito008 <rbrito9162@uta.edu.ec>
2023-09-26 17:32:50 -0500

    Enrutamiendo de las páginas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,65 @@
-function App() {
-  return <div>Blog DAS</div>;
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Router,
+  Outlet,
+} from "react-router-dom";
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Single from "./pages/Single"
+import Write from "./pages/Write"
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+const Layout = ()=>{
+  return (
+    <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+    </>
+  );
 }
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/post/:id",
+        element:<Single/>
+      },
+      {
+        path:"/write",
+        element:<Write/>
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+  </div>
+  ); 
+}
+
 
 export default App;
